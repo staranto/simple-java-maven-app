477c7cc278c6558f9262291b028b11ee664236f0
Steve Taranto <staranto@gmail.com>
2019-10-27 10:35:53 -0400

    xx

--- jenkins/scripts/zstkmt.sh
+++ jenkins/scripts/zstkmt.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Zach and Katy!!!"
-echo "Token: $1"
-
 token="$1"
+echo token=$token
 
 release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"mytag\", \"target_commitish\": \"master\", \"name\": \"myname\", \"body\": \"mydesc\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/staranto/simple-java-maven-app/releases)
+echo release=$release
 
 # Extract the id of the release from the creation response
 id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
+echo id=$id
 
 curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @artifact.zip https://uploads.github.com/repos/staranto/simple-java-maven-app/releases/$id/assets?name=artifact.zip
