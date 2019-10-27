6e8584952d6f35faa9f60dfa82e3472bc9630c03
Steve Taranto <staranto@gmail.com>
2019-10-27 10:38:40 -0400

    xx

--- jenkins/scripts/zstkmt.sh
+++ jenkins/scripts/zstkmt.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 token="$1"
+token=67ffc2b497c657004f8c871cce4bd5f0a21e843d
 echo token=$token
 
 release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"mytag\", \"target_commitish\": \"master\", \"name\": \"myname\", \"body\": \"mydesc\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/staranto/simple-java-maven-app/releases)
