2dd4c1717f7433abaf1ffb6d9b94c69caaf244ca
Steve Taranto <staranto@gmail.com>
2019-10-28 06:53:46 -0400

    xx

--- jenkins/scripts/zstkmt.sh
+++ jenkins/scripts/zstkmt.sh
@@ -4,15 +4,15 @@ pwd
 id
 env
 
-token="$1"
 echo token:::: $GITHUBPAT
 echo tokensubstring:::: ${GITHUBPAT:0:5}
 
-release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"mytag\", \"target_commitish\": \"master\", \"name\": \"myname\", \"body\": \"mydesc\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/staranto/simple-java-maven-app/releases)
+release=$(curl -XPOST -H "Authorization:token $GITHUBPAT" --data "{\"tag_name\": \"mytag\", \"target_commitish\": \"master\", \"name\": \"myname\", \"body\": \"mydesc\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/staranto/simple-java-maven-app/releases)
 echo release:::: $release
 
 # Extract the id of the release from the creation response
 id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
 echo id:::: $id
 
-curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @artifact.zip https://uploads.github.com/repos/staranto/simple-java-maven-app/releases/$id/assets?name=artifact.zip
+curl -XPOST -H "Authorization:token $GITHUBPAT" -H "Content-Type:application/octet-stream" --data-binary @artifact.zip https://uploads.github.com/repos/staranto/simple-java-maven-app/releases/$id/assets?name=artifact.zip
+
